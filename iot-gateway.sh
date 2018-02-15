954aaca3b1a78649ed99931eaa80e01c41d0c44f
alan bennett <alan@opensourcefoundries.com>
2018-02-15 15:27:05 -0700

    add getopts and usage

--- iot-gateway.sh
+++ iot-gateway.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
+usage="
+$(basename "$0") [-h] [-r] [-p] [-g] [-m] -- script to execute iot-gateway ansible playbook
+
+where:
+    -h  show this help text
+    -r  override docker registry {default=hub.foundries.io}
+    -p  override docker registry password
+    -g  override target IP/hostname
+    -m  override IP of device management system (i.e. leshan/hawkbit)
+
+"
+
+#process command-line options / overrides
+while getopts ":help:r:p:g:m:" option; do
+  case $option in
+    r ) REGISTRY=${OPTARG} ;;
+    p ) REGISTRY_PASSWD=${OPTARG} ;;
+    g ) GW_HOSTNAME=${OPTARG} ;;
+    m ) MGMT_SERVER=${OPTARG} ;;
+    * ) echo "$usage"
+        exit ;;
+  esac
+done
+shift $((OPTIND -1))
+
 # Container Registry credentials (default is OSF subscriber; can be Docker Hub)
 registry=${REGISTRY:-hub.foundries.io}
 registry_user=${REGISTRY_USER:-this-is-ignored}
-registry_passwd=${REGISTRY_PASSWD}
+registry_passwd=${REGISTRY_PASSWD:-YOUR-FOUNDRIES.IO-APP-KEY}
 registry_email=${REGISTRY_EMAIL:-docker@docker.com}
 
 # The hub variable is a bit of a misnomer.
 if [ "$registry" = "hub.foundries.io" ]; then
     if [ -z "$registry_passwd" ]; then
         echo "Error: REGISTRY_PASSWD is unset; cannot log in to hub.foundries.io."
+        echo "$usage" >&2
         exit 1
     fi
     hub=hub.foundries.io
@@ -21,6 +47,7 @@ fi
 
 if [ -z "$hub" ]; then
     echo "Error: no hub provided and no value is known."
+    echo "$usage" >&2
     exit 1
 fi
 
@@ -50,6 +77,19 @@ cloudmqttpw=${CLOUDMQTT_PASSWD:-password}
 #First argument Ansible tags
 ansibletags=${1:-gateway}
 
+#output what is about to be configuration
+echo "
+  Calling iot-gateway as:
+     GW_HOSTNAME=$hostname \\
+     MGMT_SERVER=$mgmt_server \\
+     REGISTRY=$REGISTRY \\
+     HUB=$hub \\
+     REGISTRY_PASSWD=$registry_passwd \\
+     ansibletags=$ansibletags \\
+     ./$(basename "$0")
+    "
+
+#run ansible playbook
 ansible-playbook -e "mqttuser=$cloudmqttuser mqttpass=$cloudmqttpw mqtthost=$cloudmqtthost mqttport=$cloudmqttport "\
                  -e "mgmt_server=$mgmt_server hub=$hub" \
                  -e "brokerhost=$hostname brokeruser='' brokerpw=''" \
